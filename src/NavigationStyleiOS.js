e4be678fa09381c8fd58f7c55b60c28786bd3026
Johnson Gong <jogong@ucsd.edu>
2017-02-02 20:05:53 -0800

    iOS files

---
+++ src/NavigationStyleiOS.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+var NavigationStyleiOS = StyleSheet.create({
+
+   title: {
+      paddingVertical: 10,
+      marginLeft: 20,
+      justifyContent: 'center',
+      fontSize: 16,
+      color: 'white',
+      fontWeight: 'bold',
+   },
+
+   title2: {
+      paddingVertical: 10,
+      justifyContent: 'center',
+      fontSize: 16,
+      color: 'white',
+      fontWeight: 'bold',
+   },
+
+   leftButton: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 16,
+      textAlign: 'center',
+      paddingVertical: 10,
+   },
+   
+   rightButton: {
+      color: 'white',
+      fontWeight: 'bold',
+      margin: 10,
+      fontSize: 19
+   },
+   
+});
+
+export default NavigationStyleiOS;
---
+++ src/StyleiOS.js
@@ -0,0 +1,83 @@
+import { StyleSheet } from 'react-native';
+
+var StyleiOS = StyleSheet.create({
+
+    rootContainer: {
+        flex: 1
+    },
+    // Header
+    displayContainer: {
+        flex: 1,
+        backgroundColor: '#ea665b'
+    },
+
+    headerText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    subDisplay: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'   
+    },
+
+    // Choose a room section
+    pickerContiner: {
+        color: 'white',
+        backgroundColor: '#23AFC9',
+    },
+
+    // Temperature Label Section
+    tempContainer: {
+        flex: 7, 
+        backgroundColor: '#0ba7c3'
+    },
+
+    currentTemperatureHeaderText: {
+        fontSize: 30,
+        color: 'white',
+        textAlign: 'center',
+        marginTop: 20
+    },
+
+    outsideTemperatureHeaderText: {
+        fontSize: 35,
+        color: 'white',
+        textAlign: 'center',
+    },
+
+    outsidetemperature: {
+        flex: .5,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
+    insidetemperature: {
+        flex: .5,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
+    // Temperature Section
+    temperatureButtonText: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    temperatureRow: {
+        marginTop: 30,
+    },
+
+    buttonContainer: {
+      marginTop: 100,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+
+});
+
+export default StyleiOS;
